68e93385515de89141c9fa0c13263fd2af4fd568
chdonnat <donnatchris@live.fr>
2024-08-03 20:50:56 +0200

    ok but without dico changes

--- alpha.c
+++ alpha.c
@@ -1,69 +1,111 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   alpha.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: seguinyannis <seguinyannis@student.42.f    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/03 14:26:04 by afroechl          #+#    #+#             */
-/*   Updated: 2024/08/03 16:33:57 by seguinyanni      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "struct.h"
 #include <unistd.h>
 
 int	ft_strlen(char *str);
 
-void	ft_print(char *str,t_dic *tab)
+void	ft_is_hundred(char *str, t_dic *tab)
 {
-	int	i;
 	int	j;
-	int	len; 
-		
-	len = ft_strlen(str);
-	i = 0;
 
-	if (str[i] == '0' && len == 1)
+	j = 0;
+	while (*str != tab[j].key[0])
+		j++;
+	if (*str >= '2' && *str <= '9')
 	{
-		write(1,tab[0].value, ft_strlen(tab[0].value));
+		write(1, tab[j].value, ft_strlen(tab[j].value));
+		write(1, " ", 1);
+	}
+	if (*str != '0')
+	{
+		write(1, tab[28].value, ft_strlen(tab[28].value));
+		write(1, " ", 1);
+	}
+}
+
+void	ft_is_ten(char *str, t_dic *tab)
+{
+	int	j;
+
+	if (*str >= '2' && *str <= '9')
+	{
+		j = 20;
+		while (*str != tab[j].key[0])
+			j++;
+		write(1, tab[j].value, ft_strlen(tab[j].value));
+		write(1, " ", 1);
+	}
+	if (*str == '1')
+	{
+		j = 10;
+		str++;
+		while (*str != tab[j].key[1])
+			j++;
+		write(1, tab[j].value, ft_strlen(tab[j].value));
+		write(1, " ", 1);
+		str--;
+	}
+}
+
+void	ft_is_unit(char *str, t_dic *tab)
+{
+	int	j;
+
+	str--;
+	if (*str == '1')
+	{
+		str++;
+		return;
+	}
+	str++;
+	if (*str >= '1' && *str <= '9')
+	{
+		j = 0;
+
+		while (*str != tab[j].key[0])
+			j++;
+		write(1, tab[j].value, ft_strlen(tab[j].value));
+		write(1, " ", 1);
+	}
+}
+
+void	ft_is_big(char *str, t_dic *tab, int len)
+{
+	int	j;
+
+	j = 0;
+	if (*str == '0' && str[-1] == '0' && str[-2] == '0')
+		return;
+	while (len != tab[j].key_len)
+		j++;
+	write(1, tab[j].value, ft_strlen(tab[j].value));
+	write(1, " ", 1);
+}
+
+void	ft_print(char *str, t_dic *tab)
+{
+	int		len; 
+
+	len = ft_strlen(str);
+	if (*str == '0' && len == 1)
+	{
+		write(1, tab[0].value, ft_strlen(tab[0].value));
 		write(1, "\n", 1);
 		return;
 	}
-	while(str[i] != '\0')
+	while (len > 1)
 	{
-		j = 0;
-		while (str[i] == '0' )
+		if (len % 3 == 0)
+			ft_is_hundred(str, tab);
+		if (len % 3 == 2)
+			ft_is_ten(str, tab);
+		if (len % 3 == 1)
 		{
-			i++;
-			len--;
+			ft_is_unit(str, tab);
+			ft_is_big(str, tab, len);
 		}
-		if (i%3 == 1 && str[i] == '1')
-		{
-			write(1,tab[str[i+1] - 48 + 10].value, ft_strlen(tab[str[i+1] - 48 + 10].value));
-			write(1, " ", 1);
-			i = i + 2;
-			len = len - 1;
-			while (len != tab[j].key_len)
-			{
-				j++;
-			}
-			write(1, tab[j].value,ft_strlen(tab[j].value));
-			write(1, " ", 1);
-
-			len--;
-		}
-		j = 0;
-		while(str[i] != tab[j].key[0])
-			j++;
-		write(1,tab[j].value,ft_strlen(tab[j].value));
-		write(1, " ", 1);
-		while(len != tab[j].key_len)
-			j++;
-		write(1,tab[j].value,ft_strlen(tab[j].value));
-		write(1, " ", 1);
+		str++;
 		len--;
-		i++;
 	}
+	ft_is_unit(str, tab);
 	write(1, "\n", 1);
 }
